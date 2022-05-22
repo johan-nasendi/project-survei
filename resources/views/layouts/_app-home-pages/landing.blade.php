bfdbb11a6a53b1a670cbb56deea081fe2790134b
Johan <57840332+johannasendi@users.noreply.github.com>
2022-05-22 21:02:44 +0800

    make it better

--- resources/views/layouts/_app-home-pages/landing.blade.php
+++ resources/views/layouts/_app-home-pages/landing.blade.php
@@ -7,9 +7,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <!-- CSRF Token -->
+         <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset('/img/logo.png')}}">
+
 
 		<!-- App css -->
 		<link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
--- resources/views/layouts/app-dashboard.blade.php
+++ resources/views/layouts/app-dashboard.blade.php
@@ -7,6 +7,9 @@
    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+       <!-- CSRF Token -->
+       <meta name="csrf-token" content="{{ csrf_token() }}">
+
    <title>
       {{config('app.name')}} -  @yield('title')
    </title>
