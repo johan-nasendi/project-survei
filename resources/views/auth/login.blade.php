ec61a452bd9f292909c21d23995281f95c39f3bc
Johan <57840332+johannasendi@users.noreply.github.com>
2022-05-25 01:21:23 +0800

    semua baik ada nya

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,114 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
+@extends('layouts.app-home')
 
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <title>Log In</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-        <meta content="Coderthemes" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- CSRF Token -->
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{asset('/img/logo.png')}}">
-		<!-- App css -->
-		<link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-		<link href="{{asset('/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-		<link href="{{asset('/assets/css/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
-		<link href="{{asset('/assets/css/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-		<!-- icons -->
-		<link href="{{asset('/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-    </head>
-
-    <body class="loading bg-dark authentication-bg-pattern">
-
-        <div class="account-pages mt-5 mb-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card bg-pattern">
-
-                            <div class="card-body p-4">
-
-                                <div class="text-center w-75 m-auto">
-                                    <div class="auth-logo">
-                                        <a href="#" class="logo logo-dark text-center">
-                                            <span class="logo-lg">
-                                                <img src="{{asset('/img/logo.png')}}" alt="logos" height="100">
-                                            </span>
-                                        </a>
-
-                                        <a href="index.html" class="logo logo-light text-center">
-                                            <span class="logo-lg">
-                                                <img src="{{asset('/img/logo.png')}}" alt="logos" height="100">
-                                            </span>
-                                        </a>
-                                    </div>
-                                    <h3>Selamat Datang Kembali</h3>
-                                    <p class="text-muted mb-4 mt-3">Masukkan alamat email dan kata sandi Anda..</p>
-                                </div>
-
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-
-                                    <div class="form-group mb-3">
-                                        <label for="emailaddress">Username</label>
-                                        <input id="username" type="text" class="form-control @error('username') is-invalid @enderror"
-                                        placeholder="Masukkan Username Anda" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-                                        @error('username')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                    </div>
-
-                                    <div class="form-group mb-3">
-                                        <label for="password">Password</label>
-                                        <div class="input-group input-group-merge">
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                                            placeholder="Masukkan Password Anda" name="password" required autocomplete="current-password">
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                             @enderror
-                                            <div class="input-group-append" data-password="false">
-                                                <div class="input-group-text">
-                                                    <span class="password-eye"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-warning btn-block" type="submit"> Masuk </button>
-                                    </div>
-
-                                </form>
-
-
-
-                            </div> <!-- end card-body -->
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-                        <!-- end card -->
-                    </div> <!-- end col -->
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <!-- end row -->
             </div>
-            <!-- end container -->
         </div>
-        <!-- end page -->
-
-        <!-- Vendor js -->
-        <script src="{{asset('/assets/js/vendor.min.js')}}"></script>
-
-        <!-- App js -->
-        <script src="{{asset('/assets/js/app.min.js')}}"></script>
-
-    </body>
-</html>
+    </div>
+</div>
+@endsection
