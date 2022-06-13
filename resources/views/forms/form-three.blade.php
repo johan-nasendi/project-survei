21119ad518de55a54b8e5d2201af449b61c7fdb8
Johan <57840332+johannasendi@users.noreply.github.com>
2022-06-13 11:29:12 +0800

    make it better

--- resources/views/forms/form-three.blade.php
+++ resources/views/forms/form-three.blade.php
@@ -12,7 +12,8 @@
                                 <option value="Yes" @if (old('works') == "Yes") {{ 'selected' }} @endif> Yes </option>
                                 <option value="No"  @if (old('works') == "No")  {{ 'selected' }} @endif> No </option>
                             </select>
-                            <div class="invalid-feedback">Harap memilih pilihan anda</div>
+                            <div class="text-danger">Harap memilih pilihan anda</div>
+                            {{-- <div class="invalid-feedback">Harap memilih pilihan anda</div> --}}
                             @error('works')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
